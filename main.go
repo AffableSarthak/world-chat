ad06eacd8a6b9ee81933d3c3e751e394e5a96f7c
sarthak <sarthaksharmaanuj@gmail.com>
2024-03-11 21:00:45 +0530

    Basic websocket implemention

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 // World chat is public chat forum, go to (worldchat.com/any-forum-name), anyone can join and start chatting.
@@ -10,6 +13,7 @@ func main() {
 
 	// Serve the frontend for the application
 	http.HandleFunc("GET /", serveFrontend)
+	http.HandleFunc("GET /ws", newWsConn)
 
 	// Listen and serve
 	log.Println("Server started, go to link http://localhost:8080")
@@ -23,3 +27,64 @@ func main() {
 func serveFrontend(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./frontend")
 }
+
+// WS connection properties
+var upgrade = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type (
+	worldChatJson struct {
+		RoomID   string `json:"roomId"`
+		RoomName string `json:"roomName"`
+		Message  string `json:"message"`
+	}
+)
+
+func newWsConn(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrade.Upgrade(w, r, nil)
+
+	// Error upgrading the connection to a web socket
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer conn.Close()
+	// conn is now ws connection
+
+	// Single Client Architecture, read and write to the client
+	for {
+		// Read message from the client (JSON)
+		var wsJson worldChatJson
+		err = conn.ReadJSON(&wsJson)
+
+		if err != nil {
+			// log.Println("Some error in reading the message from the frontend", err)
+
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			return
+		}
+
+		// DO something with wsJson object
+		fmt.Println(wsJson)
+
+		// Wrtie Message to the client
+		err = conn.WriteJSON(wsJson)
+		if err != nil {
+			// Error writing to the client
+			log.Println(err)
+			return
+		}
+	}
+
+}
+
+// go func (){
+// 	for {
+// 		conn.ReadJSON()
+// 	}
+// }
